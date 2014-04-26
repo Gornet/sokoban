da2e9621eb5230cba3694e1b01f6d086f6c00399
Gornet <Gornet@users.noreply.github.com>
2014-04-26 23:00:34 +0200

    Update fontion.c

--- src/fontion.c
+++ src/fontion.c
@@ -7,7 +7,7 @@ char contenu_case(plateau_t *p , int x , int y)
 }
 
 
-void tableau_tampon(plateau_t *p, plateau_t *tampon)
+void tableau_tampon(plateau_t *p, plateau_t *tampon) //Copie un tableau dans un autre
 {
 	int i = 0;
 	int j = 0;
@@ -21,71 +21,6 @@ void tableau_tampon(plateau_t *p, plateau_t *tampon)
 		}
 		i++;
 	}
-}
-
-void tampon_inverse(plateau_t *p, plateau_t *tampon)
-{
-	int i = 0;
-	int j = 0;
-
-	while(i < p->hauteur)
-	{
-		while(j < p->largeur)
-		{
-			p->tab[i * p->hauteur + j] = tampon->tab[i * p->hauteur +j];
-			j++;
-		}
-		i++;
-	}
-}
-
-void menu()
-{
-    	printf("************************************************************************************************");
-	printf("*                                 Bienvenue sur Sokoban                                        *");
-	printf("************************************************************************************************");
-    	printf("*         -Si vous souhaitez jouer , tapez 1                                                   *");
-    	printf("*         -Pour quitter , taper 2                                                              *");
-    	printf("************************************************************************************************");
-}
-
-void	alloc_memoire_partie(partie_t *partie)
-{
-	partie = ( struct partie_s * )malloc( sizeof( struct partie_s ) );
-}
-
-void	alloc_memoire_plateau(plateau_t *p)
-{
-	p = ( struct plateau_s * )malloc( sizeof( struct plateau_s ) );
-}
-
-void	alloc_memoire_tableau(plateau_t *p)
-{
-	p->tab = (char*)malloc(sizeof(char) * (p->hauteur) * (p->largeur));
-}
-
-void	detruire_tab(char *tab)
-{
-	free(tab);
-}
-
-void	detruire_plateau(plateau_t *p)
-{
-	free(p);
-}
-
-void	detruire_partie(partie_t *partie)
-{
-	free(partie);
-}
-
-void	endgame(partie_t *partie)
-{
-	detruire_tab(partie->p->tab);
-	detruire_tab(partie->tampon->tab);
-	detruire_plateau(partie->p);
-	detruire_plateau(partie->tampon);
-	detruire_partie(partie);
 }
 
 
